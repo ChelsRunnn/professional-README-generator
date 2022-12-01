5395e94b25e5d915afac5587dbe0978eaf0ad6ad
Chelsea Runacres <chelsea.runacres@gmail.com>
2022-12-01 15:34:22 -0500

    questions written and template literal for readme doc outlined

---
+++ index.js
@@ -0,0 +1,34 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {title: "What is the title of your project?"
+},
+    {descript0: "What was your motivation/Why did you build this project?", 
+     descript1: "What problem does it solve?", 
+     descript2: "What did you learn?" 
+},
+    {toC: "Would you like to include a Table of Contents? Reply Y or N"
+},
+    {installation: "How do useres install your project? Provide a step-by-step description of how to get the development environment running."
+},
+    {usage: "What instructions do useser need for usage? Include screenshots as needed."
+},
+    {credits: "Is there anyone you'd like to thank or credit? List their names here"
+},
+    {license: "What kind of license does this project have?"
+},
+    {features: "Which features would you like to highlight?"
+},
+    {howToContribute: "What's the best email for people to reach you if they have professional inqueries?"
+},
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,59 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} 
 
+  ## Description 
+    ${data.description}
+  
+  ## Table of Contents ${data.toC}
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+  
+  ## Installation 
+    To use this app, follow these instructions on installation:
+    ${data.installation} 
+
+  ## Usage 
+   This app is yours to use and explore, but if you'd like a starting point, follow these guidelines:
+   ${data.usage}
+
+  ## ${data.credits}
+    List your collaborators, if any, with links to their GitHub profiles.
+    
+    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+    
+    If you followed tutorials, include links to those here as well.
+  
+  ## License
+    This project is covered by a ${data.license} license. For more information please visit the repo's license tab.
+
+  ## Badges 
+    ${data.badges}
+    ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+    
+    Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+    
+  ## Features
+    Key features of this app include:
+    ${data.features}
+  
+  ## How to Contribute 
+    If you have any questions or would like to contribute to this project, please reach me at ${data.howToContribute}
+    
 `;
 }
 
