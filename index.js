281b6cc8fbacc6f25f6b2b2b49ce463c7437abfa
Chelsea Runacres <chelsea.runacres@gmail.com>
2022-12-03 11:19:05 -0500

    questions put in .prompt formatting to prep for inquirer

--- index.js
+++ index.js
@@ -1,34 +1,69 @@
 // TODO: Include packages needed for this application
+// const generateMarkdown = ('./utils/generateMarkdown.js');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
+inquirer
+    .prompt(questions)
+
+// use this outline for the following Qs
 const questions = [
-    {title: "What is the title of your project?"
-},
-    {descript0: "What was your motivation/Why did you build this project?", 
-     descript1: "What problem does it solve?", 
-     descript2: "What did you learn?" 
-},
-    {toC: "Would you like to include a Table of Contents? Reply Y or N"
-},
-    {installation: "How do useres install your project? Provide a step-by-step description of how to get the development environment running."
-},
-    {usage: "What instructions do useser need for usage? Include screenshots as needed."
-},
-    {credits: "Is there anyone you'd like to thank or credit? List their names here"
-},
-    {license: "What kind of license does this project have?"
-},
-    {features: "Which features would you like to highlight?"
-},
-    {howToContribute: "What's the best email for people to reach you if they have professional inqueries?"
-},
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Provide a short description of the project, include your motivation and/or the problem your app solves.',
+        name: 'description',
+    },
+    {
+        type: 'confirm',
+        message: 'Would you like to include a Table of Contents?',
+        name: 'toC',
+    },
+    {
+        type: 'input',
+        message: 'What are the steps for installation?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What instructions for use would you like to include?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'List anyone you would like to thank or acknowledge for their aid in your development process.',
+        name: 'credit',
+    },
+    {
+        type: 'checkbox',
+        message: 'What kind of license does this project have?',
+        choices: 'MIT', //TODO: add additional choices
+        name: 'license'
+    },
+    {
+        type: 'input',
+        message: 'List any features to highlight:',
+        name: 'features'
+    },
+    {
+        type: 'input',
+        message: 'What is your email',
+        name: 'howToContribute'
+    }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const fs = require('fs');
+function writeToFile(fileName, data) { }
+fs.writeFile(`${data.title}`, data)
+// pull the file name from user input of project title
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
